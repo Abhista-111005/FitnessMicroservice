5f06e7358c3999383cba2659857158a8e85b2528
Abhista <abhistachakraborty15@gmail.com>
2025-09-06 03:03:16 +0530

    Creating the Activity microservices service

---
+++ activityservice/src/main/java/com/fitness/activityservice/dto/ActivityRequest.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice.dto;
+
+import com.fitness.activityservice.model.ActivityType;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class ActivityRequest {
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additinalMetrics;
+}

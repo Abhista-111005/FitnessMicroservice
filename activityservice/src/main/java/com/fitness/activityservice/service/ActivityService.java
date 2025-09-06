edf2774cbb7288dda6bc93dee3d8c90a4abe015e
Abhista <abhistachakraborty15@gmail.com>
2025-09-06 23:25:12 +0530

    Created eureka service to make a communication between the other microservices and also made changes and updatation in the activity class

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -3,9 +3,12 @@ package com.fitness.activityservice.service;
 import com.fitness.activityservice.ActivityRepository;
 import com.fitness.activityservice.dto.ActivityRequest;
 import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +18,44 @@ public class ActivityService {
     private final ActivityRepository activityRepository;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .startTime(request.getStartTime())
+                .additionalMatrics(request.getAdditinalMetrics())
+                .build();
 
+        Activity savedActivity = activityRepository.save(activity);
+
+        return mapToResponse(savedActivity);
+
+    }
+
+    private ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMatrics(activity.getAdditionalMatrics());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatesAt(activity.getUpdatesAt());
+        return response;
+    }
+
+    public List<ActivityResponse> getUserActivity(String userId) {
+        List<Activity> activities =  activityRepository.findByUserId(userId);
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ActivityResponse getActivityById(String activityId) {
+        return activityRepository.findById(Long.valueOf(activityId))
+                .map(this::mapToResponse)
+                .orElseThrow(()->new RuntimeException("Activity not found with id: " + activityId));
     }
 }
